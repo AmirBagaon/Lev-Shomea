362b2317c6e22a2328fa9660f7af173a8e270ebb
amirbagaon <amirbaga2004@gmail.com>
2025-09-05 01:59:02 +0300

    Fix CSRF for production URL

--- levshomea/settings.py
+++ levshomea/settings.py
@@ -11,6 +11,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.run.app']
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    'https://lev-shomea-280096975885.europe-west1.run.app',  # Add this line
+    'https://*.run.app',  # This should catch it, but let's be specific
 ]
 
 INSTALLED_APPS = [
